d3804a5aec0b31cf2648303658f8c6646bcdea4d
cleiton <cleitonbrasil6@gmail.com>
2026-03-26 08:18:16 -0300

    primeiro commit

--- loja-drip/src/main.jsx
+++ loja-drip/src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import { CartProvider } from './contexts/cartContext';
-import App from './App';
+import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
